456783a5eca76966094a48d9bbe8ba3f1f1f399e
Emilse Arias <emilsearias@gmail.com>
2024-09-05 15:30:46 -0300

    agregamos validaciones en el formulario de tareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,17 +1,35 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import ListaTareas from "./ListaTareas";
+import { useForm } from "react-hook-form";
 
 const FormularioTarea = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit =(data) =>{
+ console.log(data)
+  }
+
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit)} className="mb-3">
         <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-          <Form.Control type="text" placeholder="Tarea" />
+          <Form.Control type="text" placeholder="Tarea" {...register('tarea',{
+            required: "La tarea es un dato obligatorio",
+            minLength: {
+              value: 2,
+              message: "La tarea debe tener como minimo 2 caracteres."
+            },
+            maxLength:{
+              value: 20,
+              message: "La tarea debe tener como maximo 20 caracteres."
+            }
+          })} />
           <Button variant="primary" type="submit">
             Enviar
           </Button>
         </Form.Group>
+        <Form.Text className="text-danger">{errors.tarea?.message}</Form.Text>
       </Form>
       <ListaTareas></ListaTareas>
     </section>
